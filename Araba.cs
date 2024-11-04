ccebde80623b4347768b9c9509e27b4503ba11a3
OrkunDemir5 <orkun.d1999@gmail.com>
2024-11-04 14:55:05 +0300

    Hafta5-Kapanis

---
+++ Araba.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hafta5_Kapanıs
+{
+    public class Araba
+    {
+        public DateTime UretimTarihi { get; private set; }
+        public int SeriNumb { get; set; }
+        public string Marka { get; set; }
+        public int Model { get; set; }
+        public string Renk { get; set; }
+        public int KapiSayisi { get; set; }
+
+        public Araba() 
+        {
+            UretimTarihi = DateTime.Now;// üretim tarihi anlık olarak otomatik atanır
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using Hafta5_Kapanıs;
+
+bool dogruMu = false;
+
+List<Araba> arabalar = new List<Araba>();
+
+while (!dogruMu) 
+{
+    Console.WriteLine("Araba üretmek istiyor musunuz ? e/h");
+    string cevap = Console.ReadLine().ToLower();
+
+    if (cevap == "e")
+    {
+    tekrar:
+        Araba araba = new Araba();
+
+        Console.WriteLine("Marka: ");
+        araba.Marka = Console.ReadLine();
+
+        Console.WriteLine("Model: ");
+        araba.Model = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Renk: ");
+        araba.Renk = Console.ReadLine();
+
+        Console.WriteLine("Seri Numarası: ");
+        araba.SeriNumb = Convert.ToInt32(Console.ReadLine());
+
+    KapiSayisiGir:
+        try
+        {
+            Console.WriteLine("Kapı Sayısı: ");
+            araba.KapiSayisi = Convert.ToInt16(Console.ReadLine());
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine("Geçersiz bir sayı girdiniz. Lütfen tekrar deneyiniz.");
+            goto KapiSayisiGir;
+        }
+
+        // Arabayı listeye ekleme
+        arabalar.Add(araba);
+        Console.WriteLine("Araba başarıyla eklendi.\n");
+
+        Console.WriteLine("Tekrar Araba üretmek istiyor musunuz ?");
+        string TekrarCevap = Console.ReadLine();
+
+        if (TekrarCevap == "e")
+        {
+            goto tekrar;
+        }
+
+        dogruMu = true;
+    }
+    else if (cevap == "h")
+    {
+        Console.WriteLine("ok");
+        Environment.Exit(0); // Programı kapat
+    }
+    else
+    {
+        dogruMu = false;
+    }
+
+}
+
+Console.WriteLine("Üretilen arabaların listesi");
+
+foreach(Araba araba in arabalar)
+{
+    Console.Write($"Seri Numarası: {araba.SeriNumb}\n Marka: {araba.Marka}\n Model: {araba.Model}\n Renk: {araba.Renk}\n Üretim Tarihi: {araba.UretimTarihi}\n");
+}
+
+Console.ReadKey();
+
